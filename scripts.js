365893563e8f45e2914bad51aa842694b2e44962
Dmytro <plekhotkindmytro@gmail.com>
2017-06-27 00:05:34 +0300

    Add jquery.ui

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+    $("#game-menu").on("click", function () {
+        $("#game-menu").hide("fade", {}, "slow", function () {
+            $("#game-board").show("fade", {}, "slow");
+        });
+
+    });
+});
 
 
 
