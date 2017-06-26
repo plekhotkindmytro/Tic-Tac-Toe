2643659c6bcd61a4d50aee46e061bedff422cc2b
Dmytro <plekhotkindmytro@gmail.com>
2017-06-26 19:38:53 +0300

    Test

---
+++ creatures.js
@@ -0,0 +1,161 @@
+var creatures = [{
+        "sign": "&#128125;",
+        "name": "extraterrestrial alien"
+   },
+    {
+        "sign": "&#128126;",
+        "name": "alien monster"
+   },
+    {
+        "sign": "&#128123;",
+        "name": "ghost"
+   },
+    {
+        "sign": "&#128122;",
+        "name": "japanese goblin"
+   },
+    {
+        "sign": "&#127875;",
+        "name": "jack-o-lantern"
+
+    },
+    {
+        "sign": "&#128000;",
+        "name": "rat"
+   },
+    {
+        "sign": "&#128001;",
+        "name": "mouse"
+   },
+    {
+        "sign": "&#128002;",
+        "name": "bull"
+   },
+    {
+        "sign": "&#128003;",
+        "name": "water buffalo"
+   },
+    {
+        "sign": "&#128004;",
+        "name": "cow"
+   },
+    {
+        "sign": "&#128005;",
+        "name": "tiger"
+   },
+    {
+        "sign": "&#128006;",
+        "name": "leopard"
+   },
+    {
+        "sign": "&#128007;",
+        "name": "rabbit"
+   },
+    {
+        "sign": "&#128008;",
+        "name": "cat"
+   },
+    {
+        "sign": "&#128009;",
+        "name": "dragon"
+   },
+    {
+        "sign": "&#128010;",
+        "name": "crocodile"
+   },
+    {
+        "sign": "&#128011;",
+        "name": "whale"
+   },
+    {
+        "sign": "&#128012;",
+        "name": "snail"
+   },
+    {
+        "sign": "&#128013;",
+        "name": "snake"
+   },
+    {
+        "sign": "&#128014;",
+        "name": "horse"
+   },
+    {
+        "sign": "&#128015;",
+        "name": "ram"
+   },
+    {
+        "sign": "&#128016;",
+        "name": "goat"
+   },
+    {
+        "sign": "&#128017;",
+        "name": "sheep"
+   },
+    {
+        "sign": "&#128018;",
+        "name": "monkey"
+   },
+    {
+        "sign": "&#128019;",
+        "name": "rooster"
+   },
+    {
+        "sign": "&#128020;",
+        "name": "chicken"
+   },
+    {
+        "sign": "&#128021;",
+        "name": "dog"
+   },
+    {
+        "sign": "&#128022;",
+        "name": "pig"
+   },
+    {
+        "sign": "&#128023;",
+        "name": "boar"
+   },
+    {
+        "sign": "&#128024;",
+        "name": "elephant"
+   },
+    {
+        "sign": "&#128025;",
+        "name": "octopus"
+   },
+    {
+        "sign": "&#128026;",
+        "name": "spiral shell"
+   },
+    {
+        "sign": "&#128027;",
+        "name": "bug"
+   },
+    {
+        "sign": "&#128028;",
+        "name": "ant"
+   },
+    {
+        "sign": "&#128029;",
+        "name": "honeybee"
+   },
+    {
+        "sign": "&#128030;",
+        "name": "lady beetle"
+   },
+    {
+        "sign": "&#128031;",
+        "name": "fish"
+   },
+    {
+        "sign": "&#128032;",
+        "name": "tropical fish"
+   },
+    {
+        "sign": "&#128033;",
+        "name": "blowfish"
+   },
+    {
+        "sign": "&#128034;",
+        "name": "turtle"
+}];
---
+++ flowers.js
@@ -0,0 +1,10 @@
+var flowers = [{
+    "sign": "&#127803;",
+    "name": "sunflower"
+   }, {
+    "sign": "&#127799;",
+    "name": "tulip"
+   }, {
+    "sign": "&#127801;",
+    "name": "rose"
+}];
