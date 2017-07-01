8c65ca1cec8f45f7fe88a213eafb6f9e98894dc9
Dmytro <plekhotkindmytro@gmail.com>
2017-07-01 15:51:27 +0300

    Copied front-end from Tic-Tac-Toe-Stats

--- stats.js
+++ stats.js
@@ -1,16 +1,38 @@
-var url = "/tictactoe/sea/stats";
+
 $(document).ready(function () {
 
-    $.get(url, "jsonp").done(function (data) {
-            alert("second success");
-        }, "jsonp")
-        .fail(function (data) {
-            alert("error");
-        })
-        .always(function (data) {
-            console.log(data);
-        });
-
-
+    fillTable("sea");
+    fillTable("dino");
 
 });
+
+function fillTable(edition) {
+    var template = "<tr><td><img class='character-img' src='{character-img}'></td><td>{win}</td><td>{loose}</td><td>{tie}</td></tr>"
+    $.get("/tictactoe/"+edition+"/stats", "jsonp").done(function (json) {
+        json = JSON.parse(json);
+
+        json.sort(compare);
+        function compare(a, b) {
+          if (a.win > b.win) {
+            return -1;
+          }
+          if (a.win < b.win) {
+            return 1;
+          }
+
+          return 0;
+        }
+
+        json.forEach(function(data) {
+                var result = template.replace("{character-img}", data.playerImg);
+                    result = result.replace("{win}", data.win);
+                    result = result.replace("{loose}", data.loose);
+                    result = result.replace("{tie}", data.tie);
+                    $("#"+edition+"-stats").append(result);
+
+
+        });
+
+    }, "json");
+}
+
